94744711c3396b73ebc871f2e3b0f0e3880fae63
Zack Sarver <zsarver@niyamit.com>
2022-02-03 09:32:44 -0500

    Mocking out Dossier page

--- odos-ui/src/components/Dossier/index.tsx
+++ odos-ui/src/components/Dossier/index.tsx
@@ -1,5 +1,12 @@
 import styled from "@emotion/styled";
-import { Button, Link } from "@mui/material";
+import {
+  Button,
+  Link,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@mui/material";
 import { useQuery } from "react-query";
 import { Link as RouterLink, useParams } from "react-router-dom";
 
@@ -14,6 +21,21 @@ const Container = styled.div`
   }
 `;
 
+const TextContainer = styled.div`
+  text-align: left;
+`;
+
+const CoverImage = styled.img`
+  object-fit: none;
+  align-self: flex-start;
+`;
+
+const actorsArray = [
+  { name: "Adam Driver", character: "Paterson" },
+  { name: "Golshiefteh Farahani", character: "Laura" },
+];
+const categoriesArray = ["Drama", "Romance", "Thriller"];
+
 export const Dossier = () => {
   // const [queryRefetch, setQueryFetch] = useState(true);
   const params = useParams();
@@ -27,18 +49,64 @@ export const Dossier = () => {
   if (data && !isLoading)
     return (
       <Container>
-        <img alt={data.name} src={data.url} />
-        <div>
-          <Button variant="outlined">
-            <Link component={RouterLink} to="/">
-              Back
+        <CoverImage alt={data.name} src={data.url} />
+        <TextContainer>
+          <h2>{data.name}</h2>
+          <Button variant="text">
+            <Link component={RouterLink} to="/visualization">
+              VIEW REPORT
             </Link>
           </Button>
-          <h2>{data.name}</h2>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell variant="head">Director:</TableCell>
+                <TableCell><Link component={RouterLink} to="/visualization" >Jim Jarmusch</Link></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell variant="head">Starring:</TableCell>
+                <TableCell>
+                  {actorsArray.map((a) => (
+                    <>
+                      <Link component={RouterLink} to="/visualization" >{a.name}</Link> as <Link component={RouterLink} to="/visualization" >{a.character}</Link>
+                      <br />
+                    </>
+                  ))}
+                </TableCell>
+              </TableRow>
+              <TableRow><TableCell variant="head">Year:</TableCell><TableCell>1999</TableCell></TableRow>
+              <TableRow>
+                <TableCell variant="head">{categoriesArray.length > 1? "Categories" : "Category"}:</TableCell>
+                <TableCell>
+                  {categoriesArray.join(", ")}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
           <p>
-            <span>{data.description}</span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Et
+            pharetra pharetra massa massa. Pellentesque habitant morbi tristique
+            senectus. Eget egestas purus viverra accumsan in nisl nisi
+            scelerisque. Luctus accumsan tortor posuere ac ut consequat semper
+            viverra nam. Diam vulputate ut pharetra sit amet. Arcu cursus vitae
+            congue mauris rhoncus aenean vel. Mi sit amet mauris commodo quis
+            imperdiet massa. Malesuada pellentesque elit eget gravida cum sociis
+            natoque. Odio pellentesque diam volutpat commodo sed egestas egestas
+            fringilla.</p><p>Sapien et ligula ullamcorper malesuada proin libero nunc.
+            Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper
+            dignissim cras. At urna condimentum mattis pellentesque id nibh
+            tortor id. Quis auctor elit sed vulputate mi sit amet. Viverra
+            maecenas accumsan lacus vel facilisis. Ridiculus mus mauris vitae
+            ultricies leo integer. At imperdiet dui accumsan sit amet nulla
+            facilisi. Nisi est sit amet facilisis magna etiam. Sagittis orci a
+            scelerisque purus. Ipsum faucibus vitae aliquet nec ullamcorper.
+            Purus sit amet luctus venenatis lectus magna. Platea dictumst
+            quisque sagittis purus sit amet volutpat consequat. Massa tincidunt
+            nunc pulvinar sapien et ligula ullamcorper. Sed ullamcorper morbi
+            tincidunt ornare massa eget egestas purus viverra.
           </p>
-        </div>
+        </TextContainer>
       </Container>
     );
   else return <Container />;
