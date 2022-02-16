ff26cc71f7cd6a9ef622e42c39e6cf919b18cbcd
maarten-niyamit <mottens@niyamit.com>
2022-02-15 22:45:46 -0500

    adding visualization skeleton

--- odos-ui/src/App.tsx
+++ odos-ui/src/App.tsx
@@ -49,7 +49,7 @@ function App() {
                     path="visualize"
                     element={
                       <PrivateRoute>
-                        <Visualization />
+                        <Visualization width='400' height='300' />
                       </PrivateRoute>
                     }
                   />
---
+++ odos-ui/src/components/Visualization/data.js
@@ -0,0 +1,247 @@
+export const data1 = {
+  name: 'flare',
+  children: [
+    {
+      name: 'animate',
+      children: [
+        {
+          name: 'Easing',
+          size: 17010,
+        },
+        {
+          name: 'FunctionSequence',
+          size: 5842,
+        },
+        {
+          name: 'ISchedulable',
+          size: 1041,
+        },
+        {
+          name: 'Parallel',
+          size: 5176,
+        },
+        {
+          name: 'Pause',
+          size: 449,
+        },
+        {
+          name: 'Scheduler',
+          size: 5593,
+        },
+        {
+          name: 'Sequence',
+          size: 5534,
+        },
+        {
+          name: 'Transition',
+          size: 9201,
+        },
+        {
+          name: 'Transitioner',
+          size: 19975,
+        },
+        {
+          name: 'TransitionEvent',
+          size: 1116,
+        },
+        {
+          name: 'Tween',
+          size: 6006,
+        },
+      ],
+    },
+    {
+      name: 'data',
+      children: [
+        {
+          name: 'DataField',
+          size: 1759,
+        },
+        {
+          name: 'DataSchema',
+          size: 2165,
+        },
+        {
+          name: 'DataSet',
+          size: 586,
+        },
+        {
+          name: 'DataSource',
+          size: 3331,
+        },
+        {
+          name: 'DataTable',
+          size: 772,
+        },
+        {
+          name: 'DataUtil',
+          size: 3322,
+        },
+      ],
+    },
+    {
+      name: 'display',
+      children: [
+        {
+          name: 'DirtySprite',
+          size: 8833,
+        },
+        {
+          name: 'LineSprite',
+          size: 1732,
+        },
+        {
+          name: 'RectSprite',
+          size: 3623,
+        },
+        {
+          name: 'TextSprite',
+          size: 10066,
+        },
+      ],
+    },
+    {
+      name: 'physics',
+      children: [
+        {
+          name: 'DragForce',
+          size: 1082,
+        },
+        {
+          name: 'GravityForce',
+          size: 1336,
+        },
+        {
+          name: 'IForce',
+          size: 319,
+        },
+        {
+          name: 'NBodyForce',
+          size: 10498,
+        },
+        {
+          name: 'Particle',
+          size: 2822,
+        },
+        {
+          name: 'Simulation',
+          size: 9983,
+        },
+        {
+          name: 'Spring',
+          size: 2213,
+        },
+        {
+          name: 'SpringForce',
+          size: 1681,
+        },
+      ],
+    },
+  ],
+};
+
+export const data2 = {
+  name: 'Easing',
+  children: [
+    {
+      name: 'animate',
+      children: [
+        {
+          name: 'wefwef',
+          size: 17010,
+          color: 'red',
+        },
+        {
+          name: 'fqwefqwef',
+          size: 5842,
+          color: 'red',
+        },
+        {
+          name: 'fqwfefef',
+          size: 1041,
+          color: 'red',
+        },
+      ],
+    },
+    {
+      name: 'data',
+      children: [
+        {
+          name: 'fefw',
+          size: 1759,
+          color: 'blue',
+        },
+        {
+          name: 'fwfwf',
+          size: 2165,
+          color: 'blue',
+        },
+        {
+          name: 'fefe',
+          size: 586,
+          color: 'blue',
+        },
+        {
+          name: 'wfwwef',
+          size: 3331,
+          color: 'blue',
+        },
+        {
+          name: 'fwefwefc',
+          size: 772,
+          color: 'blue',
+        },
+      ],
+    },
+    {
+      name: 'display',
+      children: [
+        {
+          name: 'fefe',
+          size: 8833,
+          color: 'purple',
+        },
+        {
+          name: 'fef',
+          size: 1732,
+          color: 'purple',
+        },
+      ],
+    },
+    {
+      name: 'physics',
+      children: [
+        {
+          name: 'wfwef',
+          size: 1082,
+          color: 'pink',
+        },
+        {
+          name: 'qwef',
+          size: 1336,
+          color: 'pink',
+        },
+        {
+          name: 'ccca',
+          size: 319,
+          color: 'pink',
+        },
+        {
+          name: 'cdcds',
+          size: 10498,
+          color: 'pink',
+        },
+        {
+          name: 'axasx',
+          size: 2822,
+          color: 'pink',
+        },
+        {
+          name: 'fwew',
+          size: 9983,
+          color: 'pink',
+        },
+      ],
+    },
+  ],
+};
