e2bb96f715e2b388ab7a37239d9d8f42a875a12c
Zack Sarver <zsarver@niyamit.com>
2022-03-10 15:46:42 -0500

    Updating package deps

--- odos-ui/src/hooks/useFetchMovieDossier.tsx
+++ odos-ui/src/hooks/useFetchMovieDossier.tsx
@@ -10,44 +10,28 @@ export const useFetchMovieDossier = (id: string | undefined, enabled= true) => {
   const { isLoading, error, data, isFetching, isError } = useQuery(
     ["dossier", id],
     () =>
-      fetch(`${import.meta.env.VITE_API_URL}/movies/${id}`)
+      fetch(`https://jd75s0eog6.execute-api.us-east-1.amazonaws.com/DEV/dossiers/0069049`)
         .then((res) => res.json())
         .then((d) => {
-          d.description = `<p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Et
-        pharetra pharetra massa massa. Pellentesque habitant morbi tristique
-        senectus. Eget egestas purus viverra accumsan in nisl nisi
-        scelerisque. Luctus accumsan tortor posuere ac ut consequat semper
-        viverra nam. Diam vulputate ut pharetra sit amet. Arcu cursus vitae
-        congue mauris rhoncus aenean vel. Mi sit amet mauris commodo quis
-        imperdiet massa. Malesuada pellentesque elit eget gravida cum sociis
-        natoque. Odio pellentesque diam volutpat commodo sed egestas egestas
-        fringilla.
-        </p>
-        <p>
-        Sapien et ligula ullamcorper malesuada proin libero nunc. Platea
-        dictumst vestibulum rhoncus est pellentesque elit ullamcorper
-        dignissim cras. At urna condimentum mattis pellentesque id nibh
-        tortor id. Quis auctor elit sed vulputate mi sit amet. Viverra
-        maecenas accumsan lacus vel facilisis. Ridiculus mus mauris vitae
-        ultricies leo integer. At imperdiet dui accumsan sit amet nulla
-        facilisi. Nisi est sit amet facilisis magna etiam. Sagittis orci a
-        scelerisque purus. Ipsum faucibus vitae aliquet nec ullamcorper.
-        Purus sit amet luctus venenatis lectus magna. Platea dictumst
-        quisque sagittis purus sit amet volutpat consequat. Massa tincidunt
-        nunc pulvinar sapien et ligula ullamcorper. Sed ullamcorper morbi
-        tincidunt ornare massa eget egestas purus viverra.
-      </p>`;
-
-          d.director = "Jim Jarmusch";
-          d.actors = [
-            { name: "Adam Driver", character: "Paterson" },
-            { name: "Golshiefteh Farahani", character: "Laura" },
-          ];
-          d.year = "1999";
-          d.genres= ["Drama", "Romance", "Thriller"];
           
+
+          d.director = d.directors;
+          delete d.directors;
+
+          d.actors = d.actors.split(",").map((a: string) => {
+            let row = a.split("as");
+            return {name: row[0], characters: JSON.parse(row[1])}
+          })
+          
+          d.year = d.releaseYear;
+          delete d.releaseYear;
+
+          d.url = d.posterUrl;
+          delete d.posterUrl;
+
+          d.genres= d.genres.split(",");
+          
+          d.plotSummary = d.plotSummary.replace("\n", "<br />")
           return d;
         }),
         {enabled: enabled}
